3cd41a6e01ba6fae72464c687c957f9d725e5042
Madison Jackson <madisonjackson@MacBook-Pro.local>
2020-09-17 18:10:09 -0400

    Added new Assignment #2

---
+++ In-class assignment 2/sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  // put setup code here
+createCanvas(windowWidth),windowHeight);
+
+
+}
+function draw() {
+  //Put draw code here
+  background(200,10,120);
+  fill(0,255,0);
+  circle(width/2,height/2,150);
+
+
+}
