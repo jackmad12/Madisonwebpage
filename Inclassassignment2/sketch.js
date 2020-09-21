c21be854b9d1ac20cf3df60d82e72198c2dda7c4
Madison Jackson <madisonjackson@MacBook-Pro.local>
2020-09-20 21:57:21 -0400

    Made an animation of a ball bouncing against the sides of the screen that would change to a new location on the screen when clicked on

--- Inclassassignment2/sketch.js
+++ Inclassassignment2/sketch.js
@@ -1,15 +1,58 @@
-
+let xpos = 550;
+let ypos = 0;
+let xballspeed = 4;
+let yballspeed = 5;
 function setup() {
   // put setup code here
 createCanvas(windowWidth,windowHeight);
 
-
 }
 
 function draw() {
-  background(200,10,120);
-  fill(0,250,200);
-  circle(100,100,70);
+  background(180,70,230);
+  fill(100,150,200);
+  circle(xpos,ypos,150);
 
+ if(mouseIsPressed & dist(xpos,ypos,mouseX,mouseY) < 100){
+xpos,ypos =random(10,100);
+
+
+
+
+ }
+
+
+
+
+
+
+
+  xpos = xpos + xballspeed;
+  ypos = ypos + yballspeed;
+
+//Check for x position
+//Check when ball crosses right side of the canvas
+if(xpos > width){
+xballspeed = -xballspeed
+
+}
+//Check when ball crosses left side of the canvas
+if(xpos < 0){
+xballspeed = -xballspeed
+
+}
+
+//Check for y position
+//Check when ball crosses the bottom of the canvas
+if (ypos > height){
+yballspeed = -yballspeed;
+
+}
+
+//Check when ball crosses the top of the canvas
+if(ypos < 0){
+yballspeed = -yballspeed;
+
+}
 
 }
