db85f4c721379cbf3fd5f600695e161088b3a117
Madison Jackson <madisonjackson@MacBook-Pro.local>
2020-10-23 19:45:25 -0400

    Made multicolored, flashing flower images that differed in appearence

---
+++ Inclassassignment6/sketch.js
@@ -0,0 +1,34 @@
+let img;
+let img1;
+let partImage;
+
+function preload(){
+
+img1= loadImage('https://i.imgur.com/9jqK7Wq.png ');
+img= loadImage('https://i.imgur.com/9jqK7Wq.png');
+
+}
+
+
+function setup() {
+  // put setup code here
+createCanvas(windowWidth,windowHeight);
+image(img,0,0);
+partImage= get(120,50,150,150);
+  background(200,10,120);
+}
+
+function draw() {
+  //Put draw code here
+  for(let i=0;i< width;i=i+150){
+    tint(random(0,255),random(0,255),random(0,255));
+      image(partImage,i,0);
+
+  }
+
+  image(img1,0,0);
+  blend(img1,250,100,250,250,1000,300,250,250,DIFFERENCE);
+
+
+
+}
